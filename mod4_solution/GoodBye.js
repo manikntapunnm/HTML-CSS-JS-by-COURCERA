a55bb9b5d450fefbae161a1b63ff646798f657eb
manikntapunnm <111174684+manikntapunnm@users.noreply.github.com>
2022-10-28 09:20:07 +0530

    Delete mod4_solution directory

--- mod4_solution/GoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var bye = new Object();
-    var Word = "GoodBye";
-    bye.speak = function speak(someName) {
-        console.log(Word + " " + someName);
-    };
-    window.bye = bye;
-})(window);
