851a00249db6ad13940e0cf4255a5700a25b8c49
manikntapunnm <111174684+manikntapunnm@users.noreply.github.com>
2022-10-28 09:20:49 +0530

    Delete Hello.js

--- Hello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var hello = new Object();
-    var Word = "Hello";
-    hello.speak = function(someName) {
-        console.log(Word + " " + someName);
-    };
-    window.hello = hello;
-})(window);
